dc341bf41370f3811c25cf69bd973a119ffe369f
JimSB <jimsuleyman@live.com>
2022-05-15 12:33:39 -0400

    /variables
    test

--- main.go
+++ main.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and press ENTER when ready."
 
 func main() {
+	///one way - declare, then assign (two steps)
+	var firstNumber int
+	var secondNumber = 5
+	var subtraction = 7
+	var answer int
 
-	fmt.Println("Hello everyone")
+	reader := bufio.NewReader(os.Stdin)
+
+	// display a welcome/instructions
+	fmt.Println("Guess the Number Game")
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println("")
+	fmt.Println("Thnk of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	//take them through the games
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply your number by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	// give them the answer
+	answer = firstNumber*secondNumber - subtraction
+	fmt.Println("The answer is", answer)
 }
