8879eb433f042cd8ce28609106d872174fff97ea
JimSB <jimsuleyman@live.com>
2022-05-15 22:38:03 -0400

    Update main.go

--- main.go
+++ main.go
@@ -16,30 +16,33 @@ func main() {
 	var subtraction = rand.Intn(8) + 2
 	var answer int
 
-	reader := bufio.NewReader(os.Stdin)
-
-	// display a welcome/instructions
-	fmt.Println("Guess the Number Game")
-	fmt.Println("---------------------")
-	fmt.Println("")
-
-	fmt.Println("Thnk of a number between 1 and 10", prompt)
-	reader.ReadString('\n')
-
-	//take them through the games
-	fmt.Println("Multiply your number by", firstNumber, prompt)
-	reader.ReadString('\n')
-
-	fmt.Println("Now multiply your number by", secondNumber, prompt)
-	reader.ReadString('\n')
-
-	fmt.Println("Divide the result by the number you originally thought of", prompt)
-	reader.ReadString('\n')
-
-	fmt.Println("Now subtract", subtraction, prompt)
-	reader.ReadString('\n')
-
-	// give them the answer
-	answer = firstNumber*secondNumber - subtraction
-	fmt.Println("The answer is", answer)
+	
 }
+
+func playTheGame(firstNumber, secondNumber, subtraction, answer int)
+reader := bufio.NewReader(os.Stdin)
+
+		// display a welcome/instructions
+		fmt.Println("Guess the Number Game")
+		fmt.Println("---------------------")
+		fmt.Println("")
+
+		fmt.Println("Thnk of a number between 1 and 10", prompt)
+		reader.ReadString('\n')
+
+		//take them through the games
+		fmt.Println("Multiply your number by", firstNumber, prompt)
+		reader.ReadString('\n')
+
+		fmt.Println("Now multiply your number by", secondNumber, prompt)
+		reader.ReadString('\n')
+
+		fmt.Println("Divide the result by the number you originally thought of", prompt)
+		reader.ReadString('\n')
+
+		fmt.Println("Now subtract", subtraction, prompt)
+		reader.ReadString('\n')
+
+		// give them the answer
+		answer = firstNumber*secondNumber - subtraction
+		fmt.Println("The answer is", answer)
